c991970f3fe14a1cbdea45ddddd51d012f68d5f3
Justin Hugon <jhugon@fnal.fake>
2016-12-13 16:54:49 -0500

    turn test pattern back off

--- femb_python/configuration/configs/adcTest.py
+++ femb_python/configuration/configs/adcTest.py
@@ -43,8 +43,8 @@ class FEMB_CONFIG(object):
         time.sleep(0.5)
 
         #Set ADC test pattern register
-        #self.femb.write_reg( 3, 0x01230000) #31 - enable ADC test pattern, 
-        self.femb.write_reg( 3, 0x81230000) #31 - enable ADC test pattern, 
+        self.femb.write_reg( 3, 0x01230000) # test pattern off
+        #self.femb.write_reg( 3, 0x81230000) # test pattern on
 
         #Set ADC latch_loc
         self.femb.write_reg( self.REG_LATCHLOC, 0x66666666)
