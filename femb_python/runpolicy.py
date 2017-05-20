7113fdd4273011ee22591eaf5b5064e48e7660da
Brett Viren <brett.viren@gmail.com>
2017-05-20 00:43:14 -0400

    Factor sumatra and non-sumatra into two classes, add unit tests.

--- femb_python/runpolicy.py
+++ femb_python/runpolicy.py
@@ -8,95 +8,45 @@ Runner for more information.
 '''
 import os
 import json
+import time
 import subprocess
 
 class Runner(object):
     '''
-    A Runner is a parameterized execution of an external program.
+    A Runner runs a program as governed by a parameter set.
 
     Parameters:
     ===========
 
-    Parameters and parameter resolution govern the detailed policy of
-    how the program is run by each call to a Runner instance.
+    A runner has canonical, default and per-call parameter sets.
+
+        - canonical parameters and their defaults are hard-coded as a
+          class attribute `canonical`.
+
+        - default parameters and their values are passed to the
+          constructor and may override canonical parameters on a
+          per-instance basis.
+
+        - call parameters and their values are passed to each call of
+          the runner instance and may override canonical and default
+          parameters for that call.
 
     Parameter resolution
     --------------------
 
-    The canonical parameters, listed below, are subject to parameter
-    resolution.  This makes use of `str.format()` using the parameter
-    set itself.
+    On each call, the parameter set is defined as above and then each
+    canonical parameter is resolved against all the parameters.  The
+    method of resolution is to call the `str.format()` method.  Thus,
+    any references to parameters like "{name}" will be replaced with
+    the corresponding value.  Canonical parameters are resolved in
+    order of their definition in the list held in the class attribute
+    `canonical`.
 
-    Per-call Parameter override
-    ---------------------------
-
-    When an instance of Runner is called an call-specific parameter
-    set may be passed.  It will be merged into (a copy of) the
-    parameter set passed during construction.
-
-    Canonical parameters
-    --------------------
-
-    Some parameters are expected and likely should be given.  If not
-    specified then defults will be guessed.  They are all subject to
-    parameter resolution as described above.  The user is allowed to
-    include additional parameters.  They will be added to the
-    `paramfile` but they will not undergo parameter resolution.
-
-        - `rundir` specifies the current working directory to use when
-          calling the program.
-
-        - `datadir` specifies where output data files should appear
-          and will be registered with Sumatra (if activated).
-
-        - `stdout` specifies the log file to which standard output
-          will be directed.
-
-        - `stderr` specifies the log file to which standard error will
-          be directed.
-
-        - `paramfile` specifies the file in which the resolved
-          parameters will be saved as a JSON text.  This variable must
-          be referenced in `argstr` if Sumatra is to discover it.
-          Note, Sumatra will actually rename this file before calling
-          the program.
-
-        - `executable` specifies the executable program file to run.
-
-        - `argstr` specifies the command line argument string to pass
-          to the program on execution.  This variable is resolved
-          last.  It should not include the program name.  It should
-          include `{paramfile}`.
-
-        - `smtname` specifies the Sumatra project name.  If no name is
-          given, the program will not be run under `smt run`.  If
-          necessary, the "{rundir}" will be initialized for Sumatra
-          (and Git).
-
-        - `smtprime` specifies a file name to use to initialize the
-          Git repository, if needed.
-
-        - `smtstore` specifies the file name to use for the Sumatra
-          store.
-
-        - `smtlabel` optional label for Sumatra
-
-        - `smtreason` optional Sumatra "reason" for the run.
     '''
 
-    # Canonical parameters, their defaults and resolution order:
-    canonical = [
-        ('rundir','.'),
-        ('datadir','.'),
-        ('stdout','{datadir}/output.log'),
-        ('stderr','{datadir}/error.log'),
-        ('paramfile','{rundir}/params.json'),
-        ('executable',None),              # required
-        ('argstr',''),
-        ('smtname',None),
-        ('smtprime','{rundir}/readme-smt.txt'),
-        ]
-
+    canonical = (
+        ('cmdline',None),
+        )
 
     def __init__(self, **params):
         '''
@@ -111,42 +61,30 @@ class Runner(object):
 
     def __call__(self, **user_params):
         '''
-        Execute program with parameters in the run directory.
+        Execute `cmdline` with parameters in the run directory.
 
         The `user_params` dictionary is merged into any defaults given
         to the constructor and then resolved.
         '''
         params = self.resolve(**user_params)
-        self.initialize(**params)
+        cmd = self.cmdline(**params)
+        self.run(cmd, **params)
 
-        # default, no Sumatra
-        cmdline = "{executable} {argstr} > {stdout} 2> {stderr}"
+    def cmdline(self, **params):
+        return params['cmdline']
 
-        smtname = params.get('smtname',None)
-        if smtname:                                # run under Sumatra
-            # warning: race condition. must run 'smt configure' just before
-            # 'smt run' as we allow data directory to change.
-            cfgcmd = "smt configure -d {datadir}".format(**params)
-            self.run(cfgcmd, params['rundir'])
-            
-            # Embed nominal command line in Sumatra command line.
-            cmdline = 'smt run -e "%s"' % cmdline
-            if 'smtlabel' in params:
-                cmdline += " -l {smtlabel}"
-            if 'smtreason' in params:
-                cmdline += " -r {smtreason}"
+    def run(self, cmd, **params):
+        '''
+        Simply pass on to exec().
 
-        # prepare param file.
-        with open(params['paramfile'], 'w') as fp:
-            fp.write(json.dumps(params, indent=4));
+        Subclass may override this to do something more interesting.
+        '''
+        self.exec(cmd)
 
-        cmdline = cmdline.format(**params)
-        self.run(cmdline, params['rundir'])
-            
     def assuredir(self, directory):
         os.makedirs(directory, exist_ok=True)
 
-    def run(self, cmdstr, cwd=".", shell=True):
+    def exec(self, cmdstr, cwd=".", shell=True):
         '''
         Wrap up the low level running of some command
         '''
@@ -160,46 +98,6 @@ class Runner(object):
             raise RuntimeError('Failed to run "%s"' % cmdstr)
 
 
-    def initialize(self, **params):
-        '''
-        Initialize rundir, optionally with Sumatra.
-        '''
-        rundir = params['rundir']
-        self.assuredir(rundir)
-        datadir = params['datadir']
-        self.assuredir(datadir)
-
-        smtname = params.get('smtname',None)
-        if not smtname:
-            return
-
-        # Initialize git. 
-
-        # Fixme: this probably does not do what the user wants if rundir is a
-        # subdir of an existing project.
-
-        dotgit = os.path.join(rundir, '.git')
-        if not os.path.exists(dotgit):
-            sc = self.run("git init", rundir)
-
-            touch = params.get("smtprime","readme.txt")
-            touch_fp = os.path.join(rundir, touch)
-            if not os.path.exists(touch_fp):
-                with open(touch_fp, "w") as fp:
-                    fp.write("Sumatra managed directory.")
-            self.run("git add %s"%touch, rundir)
-            self.run("git commit -am 'Initialize Sumatra project'", rundir)
-
-        # Initialize Sumatra.
-
-        dotsmt = os.path.join(rundir, '.smt')
-        if os.path.exists(dotsmt):
-            return
-        cmdline = "smt init -s {smtstore} -r . {smtname}".format(**params)
-        self.run(cmdline, rundir)
-        return
-                
-
     def resolve(self, **user_params):
         params = self.default_params.copy()
         params.update(user_params)
@@ -212,3 +110,193 @@ class Runner(object):
             params[name] = val
 
         return params
+
+class DirectRunner(Runner):
+    '''
+    A DirectRunner runs a program directly.
+    '''
+    canonical = (
+        # directory from which to run
+        ('rundir','.'),
+        # if defined, the parameter set will be saved to this path as a JSON file.
+        ('paramfile',None),
+        # will redirect stdout if defined
+        ('stdout',None),
+        # will redirect stderr if defined
+        ('stderr',None),
+        # The executable
+        ('executable',None),              # required
+        # The rest of the command line argument string
+        ('argstr',''),
+        )
+
+    def cmdline(self, **params):
+        '''
+        Build a command line from `executable` and `argstr` and
+        redirect output if `stdout` or `stderr` is given.
+        '''
+        cmd = "{executable} {argstr}"
+        if 'stdout' in params:
+            cmd += ' >{stdout}'
+        if 'stderr' in params:
+            cmd += ' 2>{stderr}'
+        return cmd.format(**params)
+
+    def run(self, cmd, **params):
+        '''
+        Run command built from `cmdline()` but first save param set
+        file given by `paramfile` so that it is available to the
+        command.
+        '''
+        cmd = self.cmdline(**params)
+        if not cmd:
+            raise ValueError('No executable')
+        params['cmdline'] = cmd
+
+        # prepare param file if its wanted.
+        if 'paramfile' in params:
+            paramfile = params['paramfile']
+            self.assuredir(os.path.dirname(paramfile))
+            with open(paramfile, 'w') as fp:
+                fp.write(json.dumps(params, indent=4));
+
+        self.exec(cmd)
+        
+
+        
+class SumatraRunner(Runner):
+    '''
+    A SumatraRunner runs a program as governed by a parameter set
+    under Sumatra.
+
+    The canonical parameters:
+
+        - `rundir` specifies the current working directory to use when
+          calling the program.  This is also the Sumatra workdir and
+          repository.
+
+        - `datadir` specifies where output data files should appear
+          and any found there will be registered with Sumatra.  The
+          command must take care to write there either by finding this
+          parameter in the paramfile or by including this directory in
+          some command line argument(s).
+
+        - `stdout` set an explicit log file for standard output.
+          Otherwise, Sumatra will capture.
+
+        - `stderr` set an explicit log file for standard input.
+          Otherwise, Sumatra will capture.
+
+        - `paramfile` set the file to store the parameters as JSON
+          text.  Beware: Sumatra will actually rename this file before
+          calling the program.
+
+        - `executable` specifies the program to execute.
+
+        - `argstr` gives the command line arguments to the executable.
+          It should include `{paramfile}` if Sumatra is to discover
+          it.  If `argstr` is quoted, Sumatra will not parse it (and
+          will not discover the `paramfile`).  This variable is
+          resolved last.
+
+        - `smtname` specifies the Sumatra project name.  If necessary,
+          it will be used to initialize the "{rundir}" for Sumatra
+          (and potentially for Git as well).
+
+        - `smtprime` specifies a file name to use to initialize the
+          Git repository, if needed.
+
+        - `smtstore` specifies the database URL (or file name) to use
+          for the Sumatra store.
+
+        - `smtlabel` optional label for Sumatra for each run.
+
+        - `smtreason` optional Sumatra "reason" for the run.
+    '''
+
+    canonical = (
+        ('rundir','.'),
+        ('datadir','.'),
+        ('stdout','{datadir}/output.log'),
+        ('stderr','{datadir}/error.log'),
+        ('paramfile','{rundir}/params.json'),
+        ('smtname',None),
+        ('smtstore',"{rundir}/sumatra.sqlite"),   # replace with PSQL URL
+        ('smtprime','{rundir}/readme-smt.txt'),
+        ('executable',None),              # required
+        ('argstr',''),
+        )
+
+
+    def run(self, cmd, **params):
+        '''
+        Run for Sumatra, needs potentially to initialize .smt and .git
+        directories and call 'smt configure' before finally 'smt run'.
+        '''
+        self.initialize(**params)
+
+        # warning: race condition. must run 'smt configure' just before
+        # 'smt run' as we allow data directory to change between calls.
+
+        cfgcmd = "smt configure -d {datadir}".format(**params)
+        self.exec(cfgcmd, params['rundir'])   #fixme: move to run()
+            
+        self.exec(cmd, params['rundir'])
+            
+    def cmdline(self, **params):
+        '''
+        Return a command line from the parameters
+        '''
+        smtname = params.get('smtname',None)
+
+        # Embed nominal command line in Sumatra command line.
+        cmd = 'smt run '
+        if 'smtlabel' in params:
+            cmd += " -l {smtlabel}"
+        if 'smtreason' in params:
+            cmd += " -r {smtreason}"
+        cmd += ' -e {executable} {argstr}'
+        if 'stdout' in params:
+            cmd+= ' >{stdout}'
+        if 'stderr' in params:
+            cmd += ' 2>{stderr}'
+
+        return cmd.format(**params)
+
+    def initialize(self, **params):
+        '''
+        Initialize rundir.
+        '''
+        rundir = params['rundir']
+        self.assuredir(rundir)
+        datadir = params['datadir']
+        self.assuredir(datadir)
+
+        smtname = params['smtname']       # required
+
+        # Initialize git. 
+
+        # Fixme: this probably does not do what the user wants if rundir is a
+        # subdir of an existing project.
+
+        dotgit = os.path.join(rundir, '.git')
+        if not os.path.exists(dotgit):
+            sc = self.exec("git init", rundir)
+
+            touch = params.get("smtprime","readme.txt")
+            touch_fp = os.path.join(rundir, touch)
+            if not os.path.exists(touch_fp):
+                with open(touch_fp, "w") as fp:
+                    fp.write("Sumatra managed directory.")
+            self.exec("git add %s"%touch, rundir)
+            self.exec("git commit -am 'Initialize Sumatra project'", rundir)
+
+        # Initialize Sumatra.
+
+        dotsmt = os.path.join(rundir, '.smt')
+        if os.path.exists(dotsmt):
+            return
+        cmd = "smt init -s {smtstore} -r . {smtname}".format(**params)
+        self.exec(cmd, rundir)
+        return
+                
---
+++ test/test_runpolicy.py
@@ -0,0 +1,93 @@
+#!/usr/bin/env python3
+
+
+
+from femb_python.runpolicy import Runner, DirectRunner, SumatraRunner
+
+import shutil
+import json
+import time
+import os
+
+# for real app, this unique jobid would be generated from user input 
+jobid = time.strftime("%Y%m%d-%H%M%S", time.gmtime(time.time()))
+
+basedir = '/tmp/runpolicy-test/'
+rundir = os.path.join(basedir,"rundir")
+datadir = os.path.join(basedir,"datadir-%s"%jobid)
+paramfile = os.path.join(datadir,"params.json")
+
+def clear():
+    if not os.path.exists(basedir):
+        return
+    shutil.rmtree(basedir)
+
+def make_runner(klass, **params):
+    defaults = dict(
+        executable="/bin/cp",
+        argstr="{paramfile} {datadir}/{jobid}.dat",
+        jobid=jobid,
+        paramfile=paramfile,
+        rundir=rundir,
+        datadir=datadir)
+    defaults.update(params)
+    r = klass(**defaults)
+    return r
+
+def check_dicts(d1, d2):
+    for k,v in d1.items():
+        if k in d2:
+            assert (v == d2[k])
+
+    for k,v in d2.items():
+        if k in d1:
+            assert (v == d1[k])
+
+def test_basic():
+    clear()
+    r = Runner(cmdline="/bin/pwd")
+    r()
+
+def test_direct():
+    clear()
+    r = DirectRunner(executable="/bin/ls",
+                         argstr="-l",
+                         rundir=rundir,
+                         paramfile="{rundir}/params.json",
+                         stdout="{rundir}/output.log",
+                         stderr="{rundir}/error.log")
+    r()
+
+def test_default_args_only():
+    clear()
+    r = make_runner(DirectRunner)
+    params = r.resolve()
+    r()
+    params2 = json.load(open(params['paramfile']))
+    check_dicts(params, params2)
+
+def test_per_call_args():
+    clear()
+    r = make_runner(DirectRunner)
+
+    per_call = dict(par1="foo", par2=42, par3=6.9,
+                        executable="/bin/ls",
+                        argstr="-l {paramfile}")
+
+    p1 = r.resolve(**per_call)
+    r(**per_call)
+    p2 = json.load(open(p1['paramfile']))
+    check_dicts(p1,p2)
+    
+def test_with_sumatra():
+    clear()
+    r = make_runner(SumatraRunner, smtname="test_with_sumatra")
+    r()
+    
+
+if '__main__' == __name__:
+    test_basic()
+    test_direct()
+    test_default_args_only()
+    test_per_call_args()
+    test_with_sumatra()
