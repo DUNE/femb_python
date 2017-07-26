638352aba5957edb968c038840aeeb3a40befed5
Justin Hugon <jhugon@fnal.fake>
2017-07-26 17:55:14 -0400

    show_trace_root: now can disable FFT

--- femb_python/helper_scripts/show_trace_root.py
+++ femb_python/helper_scripts/show_trace_root.py
@@ -68,18 +68,22 @@ def loadWaveform(filename):
         calibInters = None
     return result, metadata, voltages, calibSlopes, calibInters
 
-def show_trace_root(infilename,sampleMax=None,fullADCRange=False):
+def show_trace_root(infilename,sampleMax=None,fullADCRange=False,showFFT=True):
 
     waveforms, metadata, voltages, calibSlopes, calibInters = loadWaveform(infilename)
 
     fig, axs = plt.subplots(4,4)
-    fig2, axs2 = plt.subplots(4,4)
     fig.suptitle("Waveforms for socket: {}, FE: {} ADC: {}".format(metadata['iChip'],metadata['feSerial'],metadata['adcSerial']),fontsize="large")
-    fig2.suptitle("FFT for socket: {}, FE: {} ADC: {}".format(metadata['iChip'],metadata['feSerial'],metadata['adcSerial']))
+
+    
+    fig2, axs2 = None, None
+    if showFFT:
+      fig2, axs2 = plt.subplots(4,4)
+      fig2.suptitle("FFT for socket: {}, FE: {} ADC: {}".format(metadata['iChip'],metadata['feSerial'],metadata['adcSerial']))
+
     axRights = []
     for iChan in waveforms:
       ax = axs[iChan // 4][iChan % 4]
-      ax2 = axs2[iChan // 4][iChan % 4]
       waveform = waveforms[iChan]
       if fullADCRange:
         ax.set_ylim(0,4096)
@@ -101,26 +105,31 @@ def show_trace_root(infilename,sampleMax=None,fullADCRange=False):
       ax.plot(waveform,"-b")
       ax.set_title("Channel: {}".format(iChan))
 
-      N = len(waveform)
-      freqs = numpy.arange(N) / (N / 2.0)
-      fft = numpy.fft.fft(waveform)
-
-      freqs = freqs[1:N//2]
-      fft = fft[1:N//2]
-
-      fft = abs(fft*fft.conj())
-
-      ax2.plot(freqs,fft)
-      ax2.set_title("Channel: {}".format(iChan))
-      if (fft > 0.).all():
-        ax2.set_yscale('log')
-
       if iChan // 4 == 3:
         ax.set_xlabel("Time Sample")
-        ax2.set_xlabel("Frequency")
       if iChan % 4 == 0:
         ax.set_ylabel("ADC Code")
-        ax2.set_ylabel("Power")
+
+      if showFFT:
+        N = len(waveform)
+        freqs = numpy.arange(N) / (N / 2.0)
+        fft = numpy.fft.fft(waveform)
+
+        freqs = freqs[1:N//2]
+        fft = fft[1:N//2]
+
+        fft = abs(fft*fft.conj())
+
+        ax2 = axs2[iChan // 4][iChan % 4]
+        ax2.plot(freqs,fft)
+        ax2.set_title("Channel: {}".format(iChan))
+        if (fft > 0.).all():
+          ax2.set_yscale('log')
+
+        if iChan // 4 == 3:
+          ax2.set_xlabel("Frequency")
+        if iChan % 4 == 0:
+          ax2.set_ylabel("Power")
     plt.show()
 
 def main():
@@ -129,11 +138,12 @@ def main():
     parser.add_argument("infilename",help="file name to read the waveform from")
     parser.add_argument("--sampleMax",help="Show from 0 to <sampleMax> on the x-axis",type=int,default=None)
     parser.add_argument("--fullADCRange",help="Show full y-axis 0 to 4095.",action="store_true")
+    parser.add_argument("--dontShowFFT",help="Don't show FFT",action="store_true")
     
     args = parser.parse_args()
 
     try:
-        show_trace_root(args.infilename,args.sampleMax,args.fullADCRange)
+        show_trace_root(args.infilename,args.sampleMax,args.fullADCRange,not args.dontShowFFT)
     except Exception as e:
         print("{}: {}".format(type(e).__name__,e))
 
