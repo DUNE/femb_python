aae4ae17064828f755939b3debc5ce57bb9de765
Justin Hugon <jhugon@fnal.fake>
2017-07-30 13:54:37 -0400

    Rigol DP800: added 2s pause between chans; single ADC: turn on CH3 power
            Channel 3 is turned on between channel 2 and 1
            the pause is configurable in instantiation of power supply
                    control class (in the FEMB config)

--- femb_python/configuration/configs/adcTest_P1single.py
+++ femb_python/configuration/configs/adcTest_P1single.py
@@ -88,7 +88,7 @@ class FEMB_CONFIG(FEMB_CONFIG_BASE):
 
         self.NASICS = 1
         self.FUNCGENINTER = Keysight_33600A("/dev/usbtmc1",1)
-        self.POWERSUPPLYINTER = RigolDP800("/dev/usbtmc0",["CH2","CH1"]) # turn on CH2 first
+        self.POWERSUPPLYINTER = RigolDP800("/dev/usbtmc0",["CH2","CH3","CH1"]) # turn on CH2 first
         self.F2DEFAULT = 0
         self.CLKDEFAULT = "fifo"
 
