18eb4d345ab74cd26a4bbe76f2ff97b7812398da
Brian Kirby <kirbybri@gmail.com>
2018-01-24 09:47:30 -0500

    FEMB test: fixed configuration file for CE box 70, used for production FEMB test

--- femb_python/configuration/configs/wib_sbnd_v109_femb_protodune_v320_cebox_070.py
+++ femb_python/configuration/configs/wib_sbnd_v109_femb_protodune_v320_cebox_070.py
@@ -12,8 +12,8 @@ class FEMB_CONFIG(wib_sbnd_v109_femb_protodune_v320.FEMB_CONFIG):
         super().__init__()
 
         self.CLKSELECT_val_RT = 0xFF
-        self.CLKSELECT2_val_RT = 0xB3
+        self.CLKSELECT2_val_RT = 0xB2
         print("CLKSELECT at RT:",self.CLKSELECT_val_RT,self.CLKSELECT2_val_RT)
-        self.CLKSELECT_val_CT = 0xEF
-        self.CLKSELECT2_val_CT = 0xEF
+        self.CLKSELECT_val_CT = 0xFF
+        self.CLKSELECT2_val_CT = 0xFF
         print("CLKSELECT at CT:",self.CLKSELECT_val_CT,self.CLKSELECT2_val_CT)
